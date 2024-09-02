89f9ba313e91023407ec60ddd98aa603c8e0a1de
AlineUtetiwabo <alineutetiwabo088@gmail.com>
2024-09-01 22:39:14 -0400

    solution to containsDuplicate

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -4,4 +4,17 @@ from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        
+        seen = []
+        for i in nums:
+            if i in seen:
+                return True
+            else:
+                seen.append(i)
+            print(i)
+        return False  
+    
+# create an instance of the Solution class
+solution = Solution()
+
+# call the hasDuplicate with an array
+result = solution.hasDuplicate([1, 1, 2, 3, 4])
